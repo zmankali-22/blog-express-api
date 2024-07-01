3b949fb5b3c1777ff394cc9739aab261d90826e2
acharya <sav.acharya22@gmail.com>
2024-07-01 17:33:30 +1000

    login router for users

--- src/controllers/UserRouter.js
+++ src/controllers/UserRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { UserModel } = require("../models/UserModel");
+const { comparePasswords, createJwt } = require("../utils/authHelpers");
 const router = express.Router();
 
 router.get("/", async (request, response, next) => {
@@ -11,24 +12,21 @@ router.get("/", async (request, response, next) => {
 });
 
 router.get("/findById/:id", async (request, response, next) => {
-  let result = await UserModel.findById(request.params.id)
-  .exec();
+  let result = await UserModel.findById(request.params.id).exec();
   response.json({
     message: "Blog Router  Id homepage",
     result: result,
   });
 });
 router.post("/findOneQuery", async (request, response, next) => {
-  let result = await UserModel.findOne(request.body)
-  .exec();
+  let result = await UserModel.findOne(request.body).exec();
   response.json({
     message: "Blog Router  Id homepage",
     result: result,
   });
 });
 router.post("/findManyQuery", async (request, response, next) => {
-  let result = await UserModel.find(request.body)
-  .exec();
+  let result = await UserModel.find(request.body).exec();
   response.json({
     message: "User router operation",
     result: result,
@@ -71,5 +69,32 @@ router.delete("/", async (request, response, next) => {
     result: result,
   });
 });
+
+// login route
+
+router.post("/jwt", async (request, response, next) => {
+  let newJwt = "";
+  if (!request.body.password || !request.body.username) {
+    return next(new Error("Missing username or password"));
+  }
+
+  let foundUser = await UserModel.findOne({username: request.body.username}).exec();
+
+  let isPasswordCorrect = await comparePasswords(
+    request.body.password,
+    foundUser.password
+  );
+
+  if (isPasswordCorrect) {
+    newJwt = createJwt(foundUser._id);
+    response.json({
+      jwt: newJwt,
+    });
+  } else {
+    return next(new Error("Incorrect password"));
+  }
+});
+
+// validate JWT route
 
 module.exports = router;
